109f0d41b2fbfbf56cebdded949a176b3d65a5be
Vadim Ponomarov <remary300@gmail.com>
2020-06-01 10:31:13 +0300

    Updates

---
+++ precache-manifest.a555022a21a092a026295e652c28adb9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d14ac788dc32b18c5b65267f8c684bb",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "4cb62f1f6469da5cf3f4",
+    "url": "/goit-react-hw-04-movies/static/css/4.f77cd160.chunk.css"
+  },
+  {
+    "revision": "c2ac7faba9f3435bda33",
+    "url": "/goit-react-hw-04-movies/static/css/main.c4d561c2.chunk.css"
+  },
+  {
+    "revision": "048cb06929a36f8c2215",
+    "url": "/goit-react-hw-04-movies/static/js/0.78ddc653.chunk.js"
+  },
+  {
+    "revision": "0b7646a2e6923854c148",
+    "url": "/goit-react-hw-04-movies/static/js/3.cd042377.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/3.cd042377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb62f1f6469da5cf3f4",
+    "url": "/goit-react-hw-04-movies/static/js/4.fbfd996d.chunk.js"
+  },
+  {
+    "revision": "bdaf947d9fa952a88e9a",
+    "url": "/goit-react-hw-04-movies/static/js/5.d8f9c92c.chunk.js"
+  },
+  {
+    "revision": "0b645ddbda3b16a20f11",
+    "url": "/goit-react-hw-04-movies/static/js/6.abece9ee.chunk.js"
+  },
+  {
+    "revision": "c2ac7faba9f3435bda33",
+    "url": "/goit-react-hw-04-movies/static/js/main.74e989ce.chunk.js"
+  },
+  {
+    "revision": "3643bc33c0073992101c",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.16af1b4e.js"
+  }
+]);
